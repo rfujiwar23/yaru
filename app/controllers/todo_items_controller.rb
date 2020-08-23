ccbeea17bce1940e96285490987adda2ee2e1e66
Ryo Fujiwara <jjcproject@icloud.com>
2020-08-23 16:07:32 +0900

    third push

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -5,7 +5,7 @@ class TodoItemsController < ApplicationController
    redirect_to @todo_list
   end
 
-    def destroy
+  def destroy
    @todo_item = @todo_list.todo_items.find(params[:id])
    if @todo_item.destroy
     flash[:success] = "Todo List item was deleted."
@@ -15,10 +15,21 @@ class TodoItemsController < ApplicationController
    redirect_to @todo_list
   end
 
+  def complete
+ @todo_item.update_attribute(:completed_at, Time.now)
+ redirect_to @todo_list, notice: "Todo item completed"
+  end
+
+
   private
   def set_todo_list
    @todo_list = TodoList.find(params[:todo_list_id])
   end
+
+  def set_todo_item
+ @todo_item = @todo_list.todo_items.find(params[:id])
+  end
+
   def todo_item_params
    params[:todo_item].permit(:content)
   end
